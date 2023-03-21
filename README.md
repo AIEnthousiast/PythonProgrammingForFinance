acfbba07973ef2d0f1739989acc15c7f061253a0
boris <boris.ouya@gmail.com>
2023-03-21 11:25:11 +0100

    Big jump

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 #Python programming For Finance - Walkthrough with sentdex
 librairies necessary : pandas / pandas-datareader / matplotlib/ beautifulsoup4
 sklearn
+https://www.thestreet.com/dictionary/o/opening-and-closing-prices
--- main.py
+++ main.py
@@ -6,9 +6,22 @@ import pandas_datareader.data as web
 
 style.use("ggplot")
 
-start = dt.datetime(2000,1,1)
-end = dt.datetime(2016,12,31)
 
-df = web.DataReader('TSLA', 'stooq',start, end)
+#####Retrieving data
+#start = dt.datetime(2000,1,1)
+#end = dt.datetime(2016,12,31)
 
-print(df.head())
+#df = web.DataReader('TSLA', 'stooq',start, end)
+
+#print(df.head())
+
+#df.to_csv('tsla.csv')
+
+
+##We want our indexes to be dates
+df = pd.read_csv("tsla.csv",parse_dates=True,index_col=0)
+#print(df.head())
+df[["Open","Close"]].plot()
+
+plt.show()
+
